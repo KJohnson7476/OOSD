da34bb09ac9813907ab0726c3d2e638a6892085b
Keith Johnson <k.johnson1950@gmail.com>
2017-10-05 11:42:25 -0700

    setup complete

--- resources/assets/js/routes.js
+++ resources/assets/js/routes.js
@@ -1,9 +1,15 @@
 import VueRouter from 'vue-router'
 
-let routes = [{
-    path:'/',
-    component:require('./components/Example.vue')
-}];
+let routes = [
+    {
+        path:'/',
+        component:require('./components/Example.vue')
+    },
+    {
+        path:'/about',
+        component:require('./components/About.vue')
+    }
+];
 
 export default new VueRouter({
     routes
