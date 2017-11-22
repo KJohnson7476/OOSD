1e32ad0729d1886cfafff0490ae2b535d85d52bd
Keith Johnson <k.johnson1950@gmail.com>
2017-11-21 17:00:13 -0800

    refactored code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,23 +12,10 @@ class UserController extends Controller
 {
 
     public function register(Request $request) {
-//        console.log("here");
-//        console.log($request);
-//        $user = User::created($request->all());
-//        console.log($user);
 
 
         $credentials = $request->only('name', 'email', 'password');
         console.log($credentials);
-//        try {
-//            if(! $token = JWTAuth::attempt($credentials)) {
-//                return $this->response-erro(['error' => 'User credentials are not correct'], 401);
-//            }
-//        } catch (JWTException $ex) {
-//
-//        }
-//
-//        return $this->respose->array(compact('token'))->setStatusCode(200);
 
     }
 
@@ -41,16 +28,7 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-//        $this->validate($request, [
-//            'name' => 'required',
-//            'body' => 'required'
-//        ]);
-//
-//        $user = User::find(1);
-//        $user->task()->create($request->all());
-//        return "success";
-        console.log("here");
-        console.log($request);
+
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@
 |
 */
 
-//Route::get('/', function () {
-//    return view('main');
-//});
 
 Route::get('/', function () {
     return view('welcome');
@@ -23,26 +20,5 @@ Route::resource('task', 'TaskController');
 
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/home', 'HomeController@index')->name('home');
 
-//Route::get('/main', function() {
-//   return view('main');
-//});
-
-
-
-
-
-
-
-
-
-
-
-//Route::post('/registerThis', 'UserController@register');
-//Route::resource('registerThis', 'UserController');
-
-
-//Route::get('login', 'UserController@login');
-//Route::get('logout', 'UserController@logout');
